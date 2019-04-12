3a86151cb2177fefb37d593bfe9db2bd32bf135f
Dave <dave@webdave.de>
2019-04-13 00:09:02 +0200

    renamed the project from combat to sea-battle

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -4,9 +4,7 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
+      declarations: [AppComponent]
     }).compileComponents();
   }));
 
@@ -16,16 +14,18 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'combat'`, () => {
+  it(`should have as title 'sea-battle'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('combat');
+    expect(app.title).toEqual('sea-battle');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to combat!');
+    expect(compiled.querySelector('h1').textContent).toContain(
+      'Welcome to sea-battle!'
+    );
   });
 });
